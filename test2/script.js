2da76bcb44d7660936a81712b045d1aec1766c76
miyagawahiroto <100072291+miyagawahiroto@users.noreply.github.com>
2022-10-24 19:38:52 +0900

    コメント挿入

--- test2/script.js
+++ test2/script.js
@@ -1,29 +1,18 @@
 const vm = new Vue({
     el: '#app',
     data: {
+        //初期値
         results: {id:00,name:"OK"},
-        name: 'a',
     },
-
+    //ページが読み込まれた時に動く処理
     mounted() {
         axios
-                .get("http://localhost/Web/宿予約システム/test2/test.php/?a=0&timestamp=${new Date().getTime()}")
+                    //timestamp=${new Date().getTime()}を入れることで毎回違うアドレスで検索が出来るから以前のキャッシュを読み込まない
+                .get("http://localhost/Web/宿予約システム/test2/test.php/?timestamp=${new Date().getTime()}")
                 .then((response) => (this.results = response.data))
                 .catch((error) => console.log(error));
     },
     methods: {
-        getjson1: function () {
-            axios
-                .get("http://localhost/Web/宿予約システム/test2/test.php/?a=1&timestamp=${new Date().getTime()}")
-                .then((response) => (this.results = response.data))
-                .catch((error) => console.log(error));
-        },
-        getjson2: function () {
-            axios
-                .get("http://localhost/Web/宿予約システム/test2/test.php/?a=2&timestamp=${new Date().getTime()}")
-                .then((response) => (this.results = response.data))
-                .catch((error) => console.log(error));
-        }
     },
 
     computed: {
--- test2/test.php
+++ test2/test.php
@@ -1,24 +1,23 @@
 <?php
 
+//JSON形式で返却すること、文字形式がUTF-8だということの宣言
 header('Content-Type: application/json; charset=UTF-8');
 
+//データベースからユーザ情報を取得
 $pdo = new PDO('mysql:host=localhost;dbname=webdb;charset=utf8', 'webuser', 'abccsd2');
 $sql = 'SELECT * FROM user_tbl';
 $ps = $pdo->prepare($sql);
 $ps->execute();
 $search = $ps->fetchAll();
+
+//配列の宣言（無いとエラーが発生した）
 $data = array();
+
+//データベースから持ってきたデータをforeachを利用してデータの数だけ$dataに追加している
 foreach ($search as $row) {
-    array_push($data,array('id'=>$row['id'],'pass'=>$row['pass'],'username'=>$row['username'],'mail'=>$row['usermail'],'address'=>$row['address']));
+    array_push($data, array('id' => $row['id'], 'pass' => $row['pass'], 'username' => $row['username'], 'mail' => $row['usermail'], 'address' => $row['address']));
 }
-$data1 = array('value1' => array('id' => 1, 'name' => 'aaa'), 'value2' => array('id' => 2, 'name' => 'bbb'), 'value3' => array('id' => 3, 'name' => 'ccc'));
-$data2 = array('value1' => array('id' => 4, 'name' => 'AAA'), 'value2' => array('id' => 5, 'name' => 'BBB'), 'value3' => array('id' => 6, 'name' => 'CCC'));
-if(isset($_GET['a'])==true)
-if($_GET['a']==0){
-    $json_array = json_encode($data);
-}else if ($_GET['a']==1){
-    $json_array = json_encode($data1);
-}else {
-    $json_array = json_encode($data2);
-}
+//arrayの中身をJSON形式に変換している
+$json_array = json_encode($data);
+
 print $json_array;
