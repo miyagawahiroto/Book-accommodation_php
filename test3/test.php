ee77e6a46f38cd0e7fc0320391ca81a1b05b2907
miyagawahiroto <100072291+miyagawahiroto@users.noreply.github.com>
2022-11-01 18:46:51 +0900

    test3を作成

---
+++ test3/test.php
@@ -0,0 +1,23 @@
+<?php
+
+//JSON形式で返却すること、文字形式がUTF-8だということの宣言
+header('Content-Type: application/json; charset=UTF-8');
+
+//データベースからユーザ情報を取得
+$pdo = new PDO('mysql:host=mysql208.phy.lolipop.lan;dbname=LAA1418138-yadoyoyaku;charset=utf8', 'LAA1418138', 'teamd');
+$sql = 'SELECT * FROM user_tbl';
+$ps = $pdo->prepare($sql);
+$ps->execute();
+$search = $ps->fetchAll();
+
+//配列の宣言（無いとエラーが発生した）
+$data = array();
+
+//データベースから持ってきたデータをforeachを利用してデータの数だけ$dataに追加している
+foreach ($search as $row) {
+    array_push($data, array('id' => $row['id'],'username' => $row['user_name'], 'mail' => $row['user_mail'], 'address' => $row['add_ress']));
+}
+//arrayの中身をJSON形式に変換している
+$json_array = json_encode($data);
+
+print $json_array;
