dac73308eb3ef50348db9ef3f9ce34a88473fb6a
miyagawahiroto <100072291+miyagawahiroto@users.noreply.github.com>
2022-11-02 17:54:48 +0900

    test4のtest2.htmlとscript2.jsをホテルIDで検索した値のホテル情報を表示できるように改修

--- test4/script2.js
+++ test4/script2.js
@@ -3,16 +3,24 @@ const vm = new Vue({
     data: {
         //初期値
         results: {id:00,name:"OK"},
+        num:0
     },
     //ページが読み込まれた時に動く処理
     mounted() {
         axios
                     //timestamp=${new Date().getTime()}を入れることで毎回違うアドレスで検索が出来るから以前のキャッシュを読み込まない
-                .get("http://mp-class.chips.jp/test.php/?get_hotel&hotel_id=1")
+                .get("http://mp-class.chips.jp/test.php/?get_hotel&hotel_id=2")
                 .then((response) => (this.results = response.data))
                 .catch((error) => console.log(error));
     },
     methods: {
+        search(){
+            axios
+                    //timestamp=${new Date().getTime()}を入れることで毎回違うアドレスで検索が出来るから以前のキャッシュを読み込まない
+                .get("http://mp-class.chips.jp/test.php/?get_hotel&hotel_id="+this.num)
+                .then((response) => (this.results = response.data))
+                .catch((error) => console.log(error));
+        }
     },
 
     computed: {
