aa897284560539b5887c0699b6f35d3a7ba45de7
miyagawa <2101297@s.asojuku.ac.jp>
2022-11-17 18:04:06 +0900

    M2-1とM2-2のvue.jsを作成

--- html/M2-1vue.js
+++ html/M2-1vue.js
@@ -3,29 +3,33 @@ const vm = new Vue({
     data: {
         //初期値
         results: false,
-        name:'',
-        pass:'',
-        mail:'',
-        phone:'',
-        address:'',
-        gender:'',
-        age:''
+        error: false,
+        name: '',
+        pass: '',
+        mail: '',
+        phone: '',
+        address: '',
+        gender: '',
+        age: ''
     },
     //ページが読み込まれた時に動く処理
     mounted() {
     },
-    updated(){
+    updated() {
     },
     methods: {
         login() {
-            console.log("OK");
-            this.logincheck();
-            location.href = "./M2-2.html";
+            if (this.name==''||this.pass==''||this.mail==''||this.phone==''||this.address==''||this.gender==''||this.age=='') {
+                this.error=true;
+            } else {
+                this.logincheck();
+                location.href = "./M2-2.html";
+            }
 
         },
         logincheck() {
             axios
-                .get("https://mp-class.chips.jp/test.php/?create_user&name="+this.name+"&pass="+this.pass+"&mail="+this.mail+"&tel="+this.phone+"&address="+this.address+"&gender="+this.gender+"&age="+this.age)
+                .get("https://mp-class.chips.jp/test.php/?create_user&name=" + this.name + "&pass=" + this.pass + "&mail=" + this.mail + "&tel=" + this.phone + "&address=" + this.address + "&gender=" + this.gender + "&age=" + this.age)
                 .then((response) => (this.results = response.data))
                 .catch((error) => console.log(error));
         }
---
+++ html/M2-2vue.js
@@ -0,0 +1,38 @@
+const vm = new Vue({
+    el: '#app',
+    data: {
+        //初期値
+        results: false,
+        user_id: '',
+        pass: ''
+    },
+    //ページが読み込まれた時に動く処理
+    mounted() {
+    },
+    updated() {
+        if (this.results == true) {
+
+        }
+    },
+    watch: {
+        results() {
+            if (this.results == true) {
+                location.href = "./M2-1.html";
+            }
+        }
+    },
+    methods: {
+        login() {
+            this.logincheck();
+        },
+        logincheck() {
+            axios
+                .get("https://mp-class.chips.jp/test.php/?login_user&user_id=" + this.user_id + "&user_pass=" + this.pass)
+                .then((response) => (this.results = response.data))
+                .catch((error) => console.log(error));
+        }
+    },
+    computed: {
+    }
+
+});
