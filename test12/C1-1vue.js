a7a1a225408545d10d56f49b78478820079db04d
miyagawa <2101297@s.asojuku.ac.jp>
2022-12-05 10:49:37 +0900

    C1-1takeroom,bookroomまで完成

--- test12/C1-1vue.js
+++ test12/C1-1vue.js
@@ -3,7 +3,9 @@ const vm = new Vue({
     data: {
         room_id: '2',
         photo: '../photo/',
-        room: {}
+        room: {},
+        window2: false,
+        window3: false
     },
     //ページが読み込まれた時に動く処理
     mounted() {
@@ -15,19 +17,31 @@ const vm = new Vue({
     methods: {
         photo_seach() {
             axios
-                .get("https://mp-class.chips.jp/test.php/?room_photo&id="+this.room_id)
+                .get("https://mp-class.chips.jp/test.php/?room_photo&id=" + this.room_id)
                 .then((response) => (this.photo += response.data))
                 .catch((error) => console.log(error));
         },
         room_seach() {
             axios
-                .get("https://mp-class.chips.jp/test.php/?get_room&room_id="+this.room_id)
+                .get("https://mp-class.chips.jp/test.php/?get_room&room_id=" + this.room_id)
                 .then((response) => (this.room = response.data))
                 .catch((error) => console.log(error));
+        },
+        takeroom() {
+            this.window2 = !this.window2;
+        },
+        bookroom(){
+            this.window2 = false;
+            this.window3 = !this.window3;
+            setTimeout(this.sendhome, 2000);
+        },
+        sendhome(){
+            location.href = "./H1-1.html";
         }
     },
 
     computed: {
+
     }
 
 });
